f57f5f6fdf46ebe82bf86bf2057cc3db786d639a
Alex Vyatkin <alexander.vjatkin@gmail.com>
2017-01-30 15:32:13 +0300

    add webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+
+const NODE_ENV = process.env.NODE_ENV || 'development';
+const webpack = require('webpack');
+
+module.exports = {
+  context: `${__dirname}/frontend`,
+  entry: {
+    home: './home',
+    about: './about',
+  },
+  output: {
+    path: `${__dirname}/public/`,
+    filename: '[name].js',
+    library: '[name]',
+  },
+  watch: NODE_ENV === 'development',
+
+  watchOptions: {
+    aggregateTimeout: 100,
+  },
+
+  devtool: NODE_ENV === 'development' ? 'cheap-inline-module-source-map' : null,
+
+  plugins: [
+    new webpack.NoErrorsPlugin(),
+    new webpack.DefinePlugin({
+      NODE_ENV: JSON.stringify(NODE_ENV),
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'common',
+    }),
+  ],
+
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loader: 'babel-loader',
+      query: {
+        presets: ['es2015'],
+      },
+    }],
+  },
+};
+if (NODE_ENV === 'production') {
+  module.exports.plugins.push(new webpack.optimize.UglifyJsPlugin(
+    {
+      compress: {
+        warnings: false,
+        drop_console: true,
+        unsafe: true,
+      },
+    },
+  ));
+}
