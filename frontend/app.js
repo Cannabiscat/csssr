0569a3608e422135cfee31ab6aeb43f9bb9c40ef
Alex Vyatkin <alexander.vjatkin@gmail.com>
2017-01-31 01:40:35 +0300

    update app.js

--- frontend/app.js
+++ frontend/app.js
@@ -1,9 +1,15 @@
 import React, { PureComponent } from 'react';
 import ReactDOM from 'react-dom';
+import Comps from './component';
 
 class App extends PureComponent {
   render() {
-    return <h1>Hi!! I am The App!!</h1>;
+    return (
+      <div className='app'>
+        <h1>Hi!! I am The App!!</h1>
+        <Comps />
+      </div>
+    );
   }
 }
 
